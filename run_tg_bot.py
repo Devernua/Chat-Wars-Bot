e8391bd332e891b7dc8727a4340297bab437c2d2
Devernua <kukuev@phystech.edu>
2017-03-29 18:25:53 +0300

    fix

--- run_tg_bot.py
+++ run_tg_bot.py
@@ -6,5 +6,5 @@ with open('users_config.yaml', 'r') as f:
 
 for d in config:
     for user in d:
-        os.system('/home/titto/tg/bin/telegram-cli --json -d -P ' + d[user]['port'] + ' -p ' + user + ' > /dev/null &' )
-        os.system('python3 /home/titto/chat-wars-bot/main.py --admin ' + d[user]['admin'] + ' --order BlueOysterBot --port ' + d[user]['port'] + ' > /dev/null &')
+        os.system('/home/titto/tg/bin/telegram-cli --json -d -P ' + str(d[user]['port']) + ' -p ' + user + ' > /dev/null &' )
+        os.system('python3 /home/titto/chat-wars-bot/main.py --admin ' + d[user]['admin'] + ' --order BlueOysterBot --port ' + str(d[user]['port']) + ' > /dev/null &')
